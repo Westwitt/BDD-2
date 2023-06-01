4a8818db86ed6319f3d14566bff11c6590ecbde3
Westwitt <112172527+Westwitt@users.noreply.github.com>
2023-06-01 10:41:24 -0300

    Update bdEscolaIdiomas-view.sql

--- bdEscolaIdiomas/bdEscolaIdiomas-view.sql
+++ bdEscolaIdiomas/bdEscolaIdiomas-view.sql
@@ -1,6 +1,6 @@
 USE bdEscolaIdiomas
 
--- 1) Crie uma vis„o ìPreÁo_Baixoî que exiba o cÛdigo, nome do curso, carga hor·ria e o valor do curso de todos os cursos que tenha preÁo inferior ao preÁo mÈdio
+-- 1) Crie uma vis√£o ‚ÄúPre√ßo_Baixo‚Äù que exiba o c√≥digo, nome do curso, carga hor√°ria e o valor do curso de todos os cursos que tenha pre√ßo inferior ao pre√ßo m√©dio
 CREATE VIEW vwPreco_Baixo AS
 	SELECT codCurso, nomeCurso, cargaHoraria, valorCurso FROM tbCurso
 		WHERE valorCurso < (SELECT AVG(valorCurso) FROM tbCurso)
@@ -8,8 +8,31 @@ CREATE VIEW vwPreco_Baixo AS
 SELECT * FROM vwPreco_Baixo
 
 
--- 2) Usando a vis„o ìPreÁo_Baixoî, mostre todos os cursos ordenados por carga hor·ria.
+-- 2) Usando a vis√£o ‚ÄúPre√ßo_Baixo‚Äù, mostre todos os cursos ordenados por carga hor√°ria.
 SELECT * FROM vwPreco_Baixo
 	ORDER BY cargaHoraria -- DESC
 
--- 3) Crie uma vis„o ìAlunos_Turmaî que exiba o curso e a quantidade de alunos por turma.CREATE VIEW vwAlunos_Turma AS	SELECT nomeCurso, COUNT(codAluno) 'Quantidade de alunos' FROM tbCurso		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso			INNER JOIN tbMatricula ON tbMatricula.codTurma = tbTurma.codTurma					GROUP BY nomeCurso				SELECT * FROM vwAlunos_Turma-- 4) Usando a vis„o ìAlunos_Turmaî exiba a turma com maior n˙mero de alunos.SELECT * FROM vwAlunos_Turma	WHERE [Quantidade de alunos] = (SELECT MAX([Quantidade de alunos]) FROM vwAlunos_Turma)-- 5) Crie uma vis„o ìTurma_Curso que exiba o curso e a quantidade de turmasCREATE VIEW vwTurma_Curso AS	SELECT nomeCurso, COUNT(codTurma) 'Quantidade de turmas' FROM tbCurso		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso			GROUP BY nomeCursoSELECT * FROM vwTurma_Curso-- 6) Usando a vis„o ìTurma_Curso exiba o curso com menor n˙mero de turmas.SELECT * FROM vwTurma_Curso	WHERE [Quantidade de turmas] = (SELECT MIN([Quantidade de turmas]) FROM vwTurma_Curso)
+-- 3) Crie uma vis√£o ‚ÄúAlunos_Turma‚Äù que exiba o curso e a quantidade de alunos por turma.
+CREATE VIEW vwAlunos_Turma AS
+	SELECT nomeCurso, COUNT(codAluno) 'Quantidade de alunos' FROM tbCurso
+		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
+			INNER JOIN tbMatricula ON tbMatricula.codTurma = tbTurma.codTurma	
+				GROUP BY nomeCurso
+
+SELECT * FROM vwAlunos_Turma
+
+-- 4) Usando a vis√£o ‚ÄúAlunos_Turma‚Äù exiba a turma com maior n√∫mero de alunos.
+SELECT * FROM vwAlunos_Turma
+	WHERE [Quantidade de alunos] = (SELECT MAX([Quantidade de alunos]) FROM vwAlunos_Turma)
+
+-- 5) Crie uma vis√£o ‚ÄúTurma_Curso que exiba o curso e a quantidade de turmas
+CREATE VIEW vwTurma_Curso AS
+	SELECT nomeCurso, COUNT(codTurma) 'Quantidade de turmas' FROM tbCurso
+		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
+			GROUP BY nomeCurso
+
+SELECT * FROM vwTurma_Curso
+
+-- 6) Usando a vis√£o ‚ÄúTurma_Curso exiba o curso com menor n√∫mero de turmas.
+SELECT * FROM vwTurma_Curso
+	WHERE [Quantidade de turmas] = (SELECT MIN([Quantidade de turmas]) FROM vwTurma_Curso)
